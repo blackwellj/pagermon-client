e720ed17621c0e115846acac91a2cb75d2614bb3
James Blackwell <blackwellj@hotmail.co.uk>
2024-11-10 21:45:07 +0000

    Fix run.sh

--- run.sh
+++ run.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
 # Set defaults if environment variables are not provided
-API_KEY=${API_KEY:}
-PAGER_LOCATION=${PAGER_LOCATION}
+API_KEY=${API_KEY:-NOKEYSUPPLIED}
+PAGER_LOCATION=${PAGER_LOCATION:-UNKNOWN}
 FREQUENCY=${FREQUENCY:-153.0750M}
-DONGLE_SERIAL=${DONGLE_SERIAL:-00000002}
-HOSTNAME=${HOSTNAME}
+DONGLE_SERIAL=${DONGLE_SERIAL:-00000001}
+HOSTNAME=${HOSTNAME:-localhost}
 
 # Path to the client configuration file
 CONFIG_FILE="/pagermon/client/config/default.json"
@@ -26,9 +26,23 @@ cat <<EOF > $CONFIG_FILE
 }
 EOF
 
-# Use environment variables in the rtl_fm command
-rtl_fm -d "$DONGLE_SERIAL" -E dc -F 0 -A fast -f "$FREQUENCY" -s 22050 - |
-multimon-ng -q -b 1 -c -a POCSAG512 -f alpha -t raw /dev/stdin |
-node /pagermon/client/reader.js
+# Create reader.sh script to handle rtl_fm and multimon-ng processing
+READER_SCRIPT="/pagermon/client/reader.sh"
+cat <<EOF > $READER_SCRIPT
+#!/usr/bin/env bash
 
-#exec tail -f /dev/null
+# Output the online message to reader.js
+echo "POCSAG512: Address: "0000001"  Function: 0  Alpha: "$PAGER_LOCATION Online"" | node reader.js
+
+# Start rtl_fm and multimon-ng to process the pager data
+rtl_fm -d "$DONGLE_SERIAL" -E dc -F 0 -A fast -f "$FREQUENCY" -s 22050 - |
+multimon-ng -q -b 1 -c -a POCSAG512 -f alpha -t raw /dev/stdin |echo "POCSAG512: Address: "0000001"  Function: 0  Alpha: "$PAGER_LOCATION Online"" | node reader.js
+
+node /pagermon/client/reader.js
+EOF
+
+# Make the reader.sh script executable
+chmod +x $READER_SCRIPT
+
+# Execute the reader.sh script
+$READER_SCRIPT
